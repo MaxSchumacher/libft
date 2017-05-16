56240f6ff30a40dd82f8947d2a7c78466d496a0e
Max SCHUMACHER <mschumac@e1r3p5.42.fr>
2017-05-16 14:06:24 +0200

    Built & tested ft_strlen, expanded libft.h accordingly

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mschumac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/09 07:58:19 by mschumac          #+#    #+#             */
-/*   Updated: 2017/05/11 10:30:02 by mschumac         ###   ########.fr       */
+/*   Updated: 2017/05/16 13:56:06 by mschumac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,5 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *string, int needle, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+size_t	ft_strlen(const char *s);
 #endif
